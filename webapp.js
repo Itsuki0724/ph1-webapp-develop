84065b4a2a669f1dad5517f72b5475909c0163f6
土屋一貴 <itsuki_tsuchiya@tsuchiyakazutakanoMacBook-Pro.local>
2022-11-07 13:58:16 +0900

    レスポンシブ調整

--- webapp.js
+++ webapp.js
@@ -276,7 +276,9 @@ window.onload = function() {
                 }
             },
         }
-    )};
+    )
+    showProcess(today, calendar);
+};
 
 
     // $(document).ready(function(){
@@ -436,10 +438,29 @@ function openTwitter(text) {
 }
 
 
+$(function() {
+    $(".textbox-date").click(function() {
+        $(".wrapper").toggleClass("C");
+    });
+});
 
+$(function() {
+    $(".textbox-date").click(function() {
+        $(".modal-left-box").toggleClass("C");
+    });
+});
 
+$(function() {
+    $(".textbox-date").click(function() {
+        $(".modal-right-box").toggleClass("C");
+    });
+});
 
-
+$(function() {
+    $(".textbox-date").click(function() {
+        $(".modal-button").toggleClass("C");
+    });
+});
 
 // function isChecked () {
 //     if(shareCheckbox.checked){
@@ -447,4 +468,111 @@ function openTwitter(text) {
 //     }
 // }
 
+const week = ["Son", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const today = new Date();
 
+var showDate = new Date(today.getFullYear(), today.getMonth(), 1);
+
+
+// window.onload = function () {
+//     showProcess(today, calendar);
+// };
+
+function prev(){
+    showDate.setMonth(showDate.getMonth() - 1);
+    showProcess(showDate);
+}
+
+
+function next(){
+    showDate.setMonth(showDate.getMonth() + 1);
+    showProcess(showDate);
+}
+function showProcess(date) {
+    var year = date.getFullYear();
+    var month = date.getMonth();
+    document.querySelector('#header').innerHTML = year + "年 " + (month + 1) + "月";
+
+    var calendar = createProcess(year, month);
+    document.querySelector('#calendar').innerHTML = calendar;
+
+    const tag = document.querySelectorAll('td')
+    tag.forEach(e => {
+        e.addEventListener('click',() =>{
+            const year_month = document.getElementById("header");
+            // console.log(year_month.innerHTML+e.innerHTML+"日")
+            const inputDate = document.getElementById('date');
+            inputDate.value = year_month.innerHTML+e.innerHTML+"日"
+        })
+    })
+    
+}
+
+// カレンダー作成
+function createProcess(year, month) {
+    // 曜日
+    var calendar = "<table><tr class='dayOfWeek'>";
+    for (var i = 0; i < week.length; i++) {
+        calendar += "<th>" + week[i] + "</th>";
+    }
+    calendar += "</tr>";
+
+    var count = 0;
+    var startDayOfWeek = new Date(year, month, 1).getDay();
+    var endDate = new Date(year, month + 1, 0).getDate();
+    var lastMonthEndDate = new Date(year, month, 0).getDate();
+    var row = Math.ceil((startDayOfWeek + endDate) / week.length);
+
+
+    for (var i = 0; i < row; i++) {
+        calendar += "<tr>";
+
+        for (var j = 0; j < week.length; j++) {
+            if (i == 0 && j < startDayOfWeek) {
+                
+                calendar += "<td class='disabled'>" + (lastMonthEndDate - startDayOfWeek + j + 1) + "</td>";
+            } else if (count >= endDate) {
+            
+                count++;
+                calendar += "<td class='disabled'>" + (count - endDate) + "</td>";
+            } else {
+            
+                count++;
+                if(year == today.getFullYear()
+                  && month == (today.getMonth())
+                  && count == today.getDate()){
+                    calendar += "<td class='today td'>" + count + "</td>";
+                } else {
+                    calendar += "<td class='td'>" + count + "</td>";
+                }
+            }
+        }
+        calendar += "</tr>";
+    }
+    return calendar;
+}
+
+
+$(function() {
+    $(".modal-open-2").click(function() {
+        $(".wrapper").toggleClass("C");
+    });
+});
+
+$(function() {
+    $(".modal-open-2").click(function() {
+        $(".modal-left-box").toggleClass("C");
+    });
+});
+
+$(function() {
+    $(".modal-open-2").click(function() {
+        $(".modal-right-box").toggleClass("C");
+    });
+});
+
+$(function() {
+    $(".modal-open-2").click(function() {
+        $(".modal-button").toggleClass("C");
+    });
+});
